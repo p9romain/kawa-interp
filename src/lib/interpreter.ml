a2b733fc85b40d2b829b4db03af0ef8ad9aa8c8d
p9romain <romainperrod.pro9@gmail.com>
2023-11-14 18:39:37 +0100

    Time to read again the instructions to finish the main project (it's not done yet git add .)

--- src/lib/interpreter.ml
+++ src/lib/interpreter.ml
@@ -14,10 +14,8 @@ let exec_prog p =
   let global_env = Hashtbl.create 16 in
   List.iter (fun (x, _) -> Hashtbl.add global_env x Null) p.globals;
 
-  (* For inheritance, we merge all the informations we need here => don't change the algo for
-     the exec_seq and exec_meth
-   *)
-  let rec go_to_the_oldest field comp c =
+  (* Get all the methods or attributes we need to inherit *)
+  let rec get_all field comp c =
     match c.parent with
     | None -> (field c)
     | Some pt ->
@@ -33,29 +31,36 @@ let exec_prog p =
               | None -> true
               | Some _ -> false
             in
-            List.append (field c) (List.filter aux (go_to_the_oldest field comp pt_cl))
+            List.append (field c) (List.filter aux (get_all field comp pt_cl))
       end
   in
-  let f c = { c with attributes = go_to_the_oldest (fun x -> x.attributes) (fun (e, _) (x, _) -> x = e) c ; 
-                     methods = go_to_the_oldest (fun x -> x.methods) (fun e m-> m.method_name = e.method_name) c } 
+  (* Change class's attributes and methods (adding inherited ones) *)
+  let inherit c = { c with attributes = get_all (fun x -> x.attributes) (fun (e, _) (x, _) -> x = e) c ; 
+                     methods = get_all (fun x -> x.methods) (fun e m-> m.method_name = e.method_name) c } 
   in
-  let p = { p with classes = List.map f p.classes }
+  (* Apply changes on every classes *)
+  let p = { p with classes = List.map inherit p.classes }
   in
 
   let rec exec_meth f this args =
-    (* Using a caps because i'm the only on who is allowed to do it (privelege) *)
+    (* Use an upper letter because i'm the only one who is allowed to do it (privelege) *)
     Hashtbl.add args "This" (VObj this) ;
     Hashtbl.add args "Return" Null ;
+    (* Add local variables *)
     List.iter (fun (x, _) -> Hashtbl.add args x Null) f.locals ;
+    (* Execute method *)
     exec_seq f.code args ;
+    (* Get the result *)
     let res = Hashtbl.find_opt args "Return" in
+    (* Destroy the local environment *)
     Hashtbl.clear args ;
+    (* Return the result *)
     match res with
     | Some v -> v (* If unchanged (so void method), return Null *)
     | None -> Null (* Impossible because always at Null at start (but security) *)
 
   and exec_seq s local_env =
-    let rec eval_unop op e = 
+    let rec eval_unop op e =
       match op with
       | Opp ->
         begin
@@ -71,18 +76,21 @@ let exec_prog p =
         end
 
     and eval_binop op e1 e2 =
+      (* Manage operators with two numbers, and return a number *)
       let num_to_num op =
          match eval e1, eval e2 with
           | VInt n1, VInt n2 -> VInt (op n1 n2)
           | _ -> failwith "type error: integer binary operator can only be use on integers"
-      in let num_to_bool op =
-        match eval e1, eval e2 with
-          | VInt n1, VInt n2 -> VBool (op n1 n2)
-          | _ -> failwith "type error: integer binary operator can only be use on integers"
+      (* Manage operators with two booleans, and return a boolean *)
       in let bool_to_bool op =
         match eval e1, eval e2 with
           | VBool b1, VBool b2 -> VBool (op b1 b2)
           | _ -> failwith "type error: boolean binary operator can only be use on booleans"
+      (* Manage operators with two numbers, and return a boolean *)
+      in let num_to_bool op =
+        match eval e1, eval e2 with
+          | VInt n1, VInt n2 -> VBool (op n1 n2)
+          | _ -> failwith "type error: integer binary operator can only be use on integers"
       in
       match op with
       | Add -> num_to_num ( + )
@@ -112,10 +120,12 @@ let exec_prog p =
       | Or -> bool_to_bool ( || )
 
     and eval_call o m_name arg =
+      (* Check if the class exists *)
       match List.find_opt (fun cl -> cl.class_name = o.cls ) p.classes with
         | None -> failwith ("unbound value error: '" ^ o.cls ^ "' class is not declared in the scope.")
         | Some c ->
           begin
+            (* Check if the method in the class exists *)
             match List.find_opt (fun m -> m.method_name = m_name ) c.methods with
               | None -> 
                 if m_name = "constructor" then
@@ -123,8 +133,10 @@ let exec_prog p =
                 else
                   failwith ("unbound value error: '" ^ m_name ^ "' method is not declared in the class '" ^ o.cls ^ "'.")
               | Some m ->
-                let args = Hashtbl.create 1 in
+                let args = Hashtbl.create 5 in
+                (* Add all the parameters in the local environment *)
                 List.iter2 (fun e (x, _) -> Hashtbl.add args x (eval e) ) arg m.params ;
+                (* Start the method call *)
                 exec_meth m o args
           end
 
@@ -149,10 +161,12 @@ let exec_prog p =
           match m with
           | Var s ->
             begin 
+              (* Check if the variable is in the local environment *)
               match Hashtbl.find_opt local_env s with
               | Some v -> v
               | None ->
                 begin 
+                  (* Check if the variable is in the global environment *)
                   match Hashtbl.find_opt global_env s with
                   | Some v -> v
                   | None -> failwith ("unbound value error: '" ^ s ^ "' is not declared in the scope.")
@@ -160,9 +174,11 @@ let exec_prog p =
             end
           | Field (e, s) ->
             begin
+              (* Check if it's an object *)
               match eval e with
               | VObj o -> 
                 begin
+                  (* Check if the field exists *)
                   match Hashtbl.find_opt o.fields s with
                   | Some v -> v
                   | None -> failwith ("unbound value error: can't acces the field '" ^ s ^ "' in the object of type '" ^ o.cls ^ "'.")
@@ -172,30 +188,32 @@ let exec_prog p =
         end
       | This ->
         begin 
+          (* Check if the variable is in the local environment *)
           match Hashtbl.find_opt local_env "This" with
           | Some v -> v
-          | None ->
-            begin 
-              match Hashtbl.find_opt global_env "This" with
-              | Some v -> v
-              | None -> failwith ("unbound value error: .")
-            end
+          | None -> failwith ("unbound value error: 'this' is not declared in the scope.\nMaybe you are outside a class ?")
         end
       | New s ->
-        let o = { cls = s ; fields = Hashtbl.create 1 } in
+        (* Create a new object *)
+        let o = { cls = s ; fields = Hashtbl.create 5 } in
         begin
+          (* Check if the class exists *)
           match List.find_opt (fun cl -> cl.class_name = s ) p.classes with
           | None -> failwith ("unbound value error: '" ^ s ^ "' class is not declared in the scope.")
           | Some c ->
+            (* Set up all the attributes to Null *)
             List.iter (fun (x, _) -> Hashtbl.add o.fields x Null ) c.attributes ;
             VObj o
         end
       | NewCstr (s, el) ->
+        (* Create a new object *)
         let VObj(o) = eval (New s) in
+        (* Call the constructor *)
         let _ = eval_call o "constructor" el in
         VObj o
       | MethCall (e, s, el) ->
         begin
+          (* Check if it's an object *)
           match eval e with
           | VObj o -> eval_call o s el
           | _ -> failwith "type error: can't access to a field of a non-object"
@@ -214,11 +232,13 @@ let exec_prog p =
         begin
           match m with
           | Var s -> 
-            begin 
+            begin
+              (* Check if the variable is in the local environment *)
               match Hashtbl.find_opt local_env s with
               | Some _ -> Hashtbl.add local_env s (eval e)
               | None ->
-                begin 
+                begin
+                  (* Check if the variable is in the global environment *)
                   match Hashtbl.find_opt global_env s with
                   | Some _ -> Hashtbl.add global_env s (eval e)
                   | None -> failwith ("unbound value error: '" ^ s ^ "' is not declared in the scope.")
@@ -226,9 +246,11 @@ let exec_prog p =
             end
           | Field (e', s) ->
             begin
+              (* Check if it's an object *)
               match eval e' with
               | VObj o -> 
                 begin
+                  (* Check if the field exists *)
                   match Hashtbl.find_opt o.fields s with
                   | Some _ -> Hashtbl.add o.fields s (eval e)
                   | None -> failwith ("unbound value error: can't acces the field '" ^ s ^ "' in the object of type '" ^ o.cls ^ "'.")
@@ -250,8 +272,8 @@ let exec_prog p =
           | _ -> failwith "type error: can't evaluate anything else than a boolean as a condition test"
         end
       | DoWhile (s, w) ->
-        exec_seq s local_env ;
-        exec_seq [ w ] local_env
+        exec_seq s local_env ; (* do *)
+        exec_seq [ w ] local_env (* while *)
       | Cond c -> exec_cond c
       | Return e -> Hashtbl.add local_env "Return" (eval e)
       | Expr e -> 
